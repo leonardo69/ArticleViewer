a00a31555614a8e8df9ad8d4b9e017ae3f5922c7
Leonardo <sacreder@gmail.com>
2017-04-07 10:11:10 +0300

    Created template app

---
+++ ArticleViewer/ArticleViewer/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ArticleViewer.Startup))]
+namespace ArticleViewer
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
